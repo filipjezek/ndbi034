101d38e59bb49f1be6589db6c239a8df216a88f9
Filip Ježek <filip.jezek@gmail.com>
2024-01-08 12:26:01 +0100

    serve frontend from flask

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,3 +4,10 @@ from .server_api import api
 import flask
     
 app.register_blueprint(api, url_prefix='/api/')
+
+@app.errorhandler(404)
+def serve_spa(error: NotFound):
+    try:
+        return flask.send_from_directory(app.static_folder + '/frontend_dist/browser', flask.request.path[1:])
+    except NotFound:
+        return app.send_static_file('frontend_dist/browser/index.html')
